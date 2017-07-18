333c71b8fcde45ba2af3190b6d9a596575c9e9bc
manurp <manojrpoojary@gmail.com>
2017-07-18 14:48:18 +0530

    Phone number parsing regular expression patterens

--- web_data/regular_expression.py
+++ web_data/regular_expression.py
@@ -93,3 +93,48 @@ $ 					# end of string
 
 print(re.search(pattern,'MCDIV',re.VERBOSE)) #re.VERBOSE is necessary which is a constant
 print(re.search(pattern,'MCDIV'))   #Assumes as compact regular expression when not specified as verbose
+
+print("\nParsing Phone Numbers\n")
+
+phonePattern = re.compile(r'^(\d{3})-(\d{3})-(\d{4})$') # (\d{3}) means Putting it all in parentheses means “match exactly three numeric 
+														#digits, and then remember them as a group that I can ask for later”.
+print(phonePattern.search('800-555-1212').groups())
+# print(phonePattern.search('800-555-1212-1234').groups())
+phonePattern = re.compile(r'^(\d{3})-(\d{3})-(\d{4})-(\d+)$')
+print(phonePattern.search('800-555-1212-1234').groups())
+# print(phonePattern.search('800-555-1212').groups())
+
+phonePattern = re.compile(r'^(\d{3})\D+(\d{3})\D+(\d{4})\D+(\d+)$') #\D any not digit character + means 1 or more
+print(phonePattern.search('800-555-1212-1234').groups()) 
+print(phonePattern.search('800.555 1212 1234').groups())
+# phonePattern.search('80055512121234').groups()
+# print(phonePattern.search('800-555-1212').groups())
+
+phonePattern = re.compile(r'^(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$') # * means 0 or more
+print(phonePattern.search('800-555-1212-1234').groups()) 
+print(phonePattern.search('800.555 1212 1234').groups())
+print(phonePattern.search('80055512121234').groups())
+print(phonePattern.search('800-555-1212').groups())
+# phonePattern.search('(800)5551212 x1234').groups()
+
+phonePattern = re.compile(r'^\D*(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$')
+print(phonePattern.search('(800)5551212 x1234').groups())
+# phonePattern.search('work 1-(800) 555.1212 #1234').groups()
+
+phonePattern = re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$')
+print(phonePattern.search('work 1-(800) 555.1212 #1234').groups())
+
+phonePattern = re.compile(r'''
+								# don't match beginning of string, number can start anywhere
+(\d{3}) 						# area code is 3 digits (e.g. '800')
+\D* 							# optional separator is any number of non-digits
+(\d{3}) 						# trunk is 3 digits (e.g. '555')
+\D* 							# optional separator
+(\d{4})							# rest of number is 4 digits (e.g. '1212')
+\D* 							# optional separator
+(\d*) 							# extension is optional and can be any number of digits
+$ 								# end of string
+''', re.VERBOSE)
+print(phonePattern.search('800-555-1212').groups())
+print(phonePattern.search('(800)5551212 x1234').groups())
+print(phonePattern.search('work 1-(800) 555.1212 #1234').groups())
