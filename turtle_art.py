94d3205a159c2f149eee805d0be07e46c73428e4
manurp <manojrpoojary@gmail.com>
2017-07-21 21:56:54 +0530

    some improvements

--- turtle_art.py
+++ turtle_art.py
@@ -4,16 +4,16 @@ def draw_art():
 	window.bgcolor('red')
 
 	brad = turtle.Turtle()
-	brad.color('#','blue')
+	brad.color('yellow','blue')
 	brad.shape('turtle')
-	brad.speed(20)
+	brad.speed(15)
+	
 	brad.left(10)
-
 	for _ in range(36):
 		brad.right(10)
 		for i in range(4):
 			brad.forward(100)
 			brad.right(90)
-
 	window.exitonclick()
+
 draw_art()
