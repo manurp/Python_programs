b5fe483c69277cb9a1fd9585ad7b76627921011a
manurp <manojrpoojary@gmail.com>
2017-07-23 10:17:08 +0530

    rename

---
+++ plural/plural_list_of_patterns.py
@@ -0,0 +1,28 @@
+#CLOSURE
+import re
+
+def build_match_and_apply_functions(pattern, search, replace):
+	def matches_rule(word):
+		return re.search(pattern, word)
+	def apply_rule(word): 
+		return re.sub(search, replace, word)
+	return (matches_rule, apply_rule)
+
+patterns = \
+(
+('[sxz]$', '$', 'es'),
+('[^aeioudgkprt]h$', '$', 'es'),
+('(qu|[^aeiou])y$', 'y$', 'ies'),
+('$', '$', 's') 
+)
+rules = [build_match_and_apply_functions(pattern, search, replace) for (pattern, search, replace) in patterns]
+
+def plural(noun):
+	for matches_rule, apply_rule in rules:
+		if matches_rule(noun):
+			return apply_rule(noun)
+
+print(plural('kid'))
+print(plural('agency'))
+print(plural('bass'))
+print(plural('rash'))
