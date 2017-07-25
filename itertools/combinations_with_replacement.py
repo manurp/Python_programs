2b8e600b4bfef0bb9dda8dbf890e0ef2b2406f0a
manurp <manojrpoojary@gmail.com>
2017-07-25 09:10:43 +0530

    Combinations with replacement

--- itertools/combinations_with_replacement.py
+++ itertools/combinations_with_replacement.py
@@ -1,4 +1,9 @@
 from itertools import combinations_with_replacement
 
-for i in combinations_with_replacement('HACK',2):
-	print(i)
+s,n = input().split()
+print(*[''.join(j) for j in combinations_with_replacement(sorted(s),int(n))],sep='\n')
+
+#Alternatively
+# for i in combinations_with_replacement(sorted(s),int(n)):
+# 	print(''.join(i))
+
