69058319d7fb2a88fed0f034839e0c1c31cfbeb1
manurp <manojrpoojary@gmail.com>
2017-07-28 08:43:44 +0530

    Unknown change

--- employee.py
+++ employee.py
@@ -10,22 +10,26 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
 
+
 class Employee:
-	def __init__(self,first,last):
-		self.first = first
-		self.last = last
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
 
-		logger.info('Created employee: {} - {}'.format(self.fullname,self.email))
+        logger.info('Created employee: {} - {}'.format(self.fullname, self.email))
 
-	@property
-	def fullname(self):
-		return '{} {}'.format(self.first,self.last)
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
 
-	@property
-	def email(self):
-		return '{}.{}@email.com'.format(self.first,self.last)
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
 
-emp1 = Employee('Manoj','Poojari')
-emp2 = Employee('Ravi','Kumar')
-emp3 = Employee('Shamantha','Ravi')
-emp4 = Employee('Bhoomika','Poojari')
+
+emp1 = Employee('Manoj', 'Poojari')
+emp2 = Employee('Ravi', 'Kumar')
+emp3 = Employee('Shamantha', 'Ravi')
+emp4 = Employee('Bhoomika', 'Poojari')
+
+# print(emp1)
