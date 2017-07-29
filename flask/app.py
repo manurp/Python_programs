e45b4f5b841e62610b08e6147cf00cc594a76d88
manurp <manojrpoojary@gmail.com>
2017-07-29 09:35:35 +0530

    Some more pages

--- flask/app.py
+++ flask/app.py
@@ -8,5 +8,20 @@ def index():
     return "<h1 style='color: green'>Hello Flask</h1> <p>This is the home page</p>"
 
 
+@app.route('/manu')
+def hello():
+    return '<h2>Hello manu</h2>'
+
+
+@app.route('/profile/<username>')
+def say_hi(username):
+    return '<h3>Hi {}</h3>'.format(username)
+
+
+@app.route('/post/<int:post_id>')
+def print_id(post_id):
+    return '<p style="color:blue">The id is {}</p>'.format(post_id)
+
+
 if __name__ == "__main__":
     app.run()
