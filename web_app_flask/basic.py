5f7d32d296ad6fd337b88a1baf20c368b4208158
manurp <manojrpoojary@gmail.com>
2018-05-05 22:39:28 +0530

    register page using forms

--- web_app_flask/basic.py
+++ web_app_flask/basic.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'manurpoojari12manu'
 
 posts_list = [
     {
@@ -32,6 +34,18 @@ def posts():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = RegistrationForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
