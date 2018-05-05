86276b08d91b9527797330f33d7a56255813cb42
manurp <manojrpoojary@gmail.com>
2018-05-05 10:40:31 +0530

    first page using flask

---
+++ web_app_flask/first.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "<h1>Home page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>about page</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
