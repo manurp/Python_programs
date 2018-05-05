6fdaa12af2b66f102431f7414fd0cdd4c147b865
manurp <manojrpoojary@gmail.com>
2018-05-05 13:43:38 +0530

    After adding the layout template

--- web_app_flask/basic.py
+++ web_app_flask/basic.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-posts = [
+posts_list = [
     {
         'author': 'Manoj',
-        'tilte': 'FIrst blog',
+        'title': 'FIrst blog',
         'date': 'April 17 2018',
         'content': 'first blog using python and flask'
     },
     {
         'author': 'Bhoomi',
-        'tilte': 'Secod blog',
+        'title': 'Secod blog',
         'date': 'April 17 2018',
         'content': 'Second blog using html and flask'
     }
@@ -26,7 +26,7 @@ def home():
 
 @app.route("/posts")
 def posts():
-    return render_template('posts.html', posts=posts)
+    return render_template('posts.html', posts=posts_list, title='Posts')
 
 
 @app.route("/about")
