536d9833a7db6edaf6232aa0039c741cc63fc72a
manurp <manojrpoojary@gmail.com>
2018-05-05 12:09:59 +0530

    some dynamic posts

--- web_app_flask/basic.py
+++ web_app_flask/basic.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Manoj',
+        'tilte': 'FIrst blog',
+        'date': 'April 17 2018',
+        'content': 'first blog using python and flask'
+    },
+    {
+        'author': 'Bhoomi',
+        'tilte': 'Secod blog',
+        'date': 'April 17 2018',
+        'content': 'Second blog using html and flask'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
@@ -9,9 +24,14 @@ def home():
     return render_template('home.html')
 
 
+@app.route("/posts")
+def posts():
+    return render_template('posts.html', posts=posts)
+
+
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
