d2f92cc2d27ed58da52aec30759d31108c010339
manurp <manojrpoojary@gmail.com>
2018-05-07 16:27:01 +0530

    account section and some cleanup

--- web_app_flask/flaskblog/__init__.py
+++ web_app_flask/flaskblog/__init__.py
@@ -10,5 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'  #login is the function name of the route
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
