90fce86d5af4a0260a3390aa235a8aed0d5d4781
manurp <manojrpoojary@gmail.com>
2018-05-08 12:09:27 +0530

    user profile picture and updating information

--- web_app_flask/flaskblog/routes.py
+++ web_app_flask/flaskblog/routes.py
@@ -1,10 +1,13 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.models import User, Post
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts_list = [
+posts = [
     {
         'author': 'Manoj',
         'title': 'First blog',
@@ -23,12 +26,7 @@ posts_list = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts_list)
-
-
-@app.route("/posts")
-def posts():
-    return render_template('posts.html', posts=posts_list, title='Posts')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
@@ -46,7 +44,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Your account has been created! You are now able to login', 'success')
+        flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -61,23 +59,47 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            flash('You have been logged in!!', 'success')
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('login Unsuccessful. Please check email and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
 @app.route("/logout")
 def logout():
     logout_user()
-    flash('logout Successful!!', 'success')
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
