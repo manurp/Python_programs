b25e7d9786a6d7a9f1f63c551d002b07a4f906a8
manurp <manojrpoojary@gmail.com>
2018-05-10 19:29:58 +0530

    added posts page of individuals

--- web_app_flask/flaskblog/routes.py
+++ web_app_flask/flaskblog/routes.py
@@ -11,8 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    page = request.args.get('page',1,type=int)
-    posts = Post.query.paginate(page=page, per_page=2) #per_page can be changed to 5
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)  # per_page can be changed to 5
     return render_template('home.html', posts=posts)
 
 
@@ -129,6 +129,7 @@ def update_post(post_id):
         form.content.data = post.content
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
 
+
 @app.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(post_id):
@@ -139,3 +140,12 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!!', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)  # per_page can be changed to 5
+    return render_template('user_posts.html', posts=posts, user=user)
